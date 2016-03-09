f57f6b9a56bbb5359495264e27fe9735a755bff1
Decors <decors1215@gmail.com>
2016-03-09 18:57:02 +0900

    Move files

---
+++ functions/set_color_custom.fish
@@ -0,0 +1,24 @@
+function set_color_custom
+    set -U fish_color_normal                normal
+    set -U fish_color_command               magenta
+    set -U fish_color_param                 cyan
+    set -U fish_color_redirection           normal
+    set -U fish_color_comment               red
+    set -U fish_color_error                 red --bold
+    set -U fish_color_escape                cyan
+    set -U fish_color_operator              cyan
+    set -U fish_color_end                   white
+    set -U fish_color_quote                 brown
+    set -U fish_color_autosuggestion        black
+    set -U fish_color_valid_path            --underline
+    set -U fish_color_cwd                   green
+    set -U fish_color_cwd_root              red
+    set -U fish_color_match                 cyan
+    set -U fish_color_search_match          --background=blue
+    set -U fish_color_selection             --background=blue
+    set -U fish_pager_color_prefix          cyan
+    set -U fish_pager_color_completion      normal
+    set -U fish_pager_color_description     black
+    set -U fish_pager_color_progress        cyan
+    set -U fish_color_history_current       cyan
+end
